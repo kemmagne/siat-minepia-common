f094e1ff61e7f0eb0cff030eb155a78eb0f6d37a
Ulrich ETEME <ulrichETEME@guichetunique.org>
2017-04-21 18:43:08 +0100

    Ajout de la méthode findByNumeroDemandeAndFileTypeGuce

--- src/main/java/org/guce/siat/common/dao/FileDao.java
+++ src/main/java/org/guce/siat/common/dao/FileDao.java
@@ -10,131 +10,115 @@ import org.guce.siat.common.model.FileFieldValue;
 import org.guce.siat.common.model.User;
 import org.guce.siat.common.utils.enums.FileTypeCode;
 
-
 /**
  * The Interface FileDao.
  */
-public interface FileDao extends AbstractJpaDao<File>
-{
-	/**
-	 * Find file item field value by field code.
-	 *
-	 * @param file
-	 *           the file
-	 * @param fieldCode
-	 *           the field code
-	 * @return the file item field value
-	 */
-	FileFieldValue findFileFieldValueByFieldCode(File file, String fieldCode);
+public interface FileDao extends AbstractJpaDao<File> {
 
-	/**
-	 * Quick search.
-	 *
-	 * @param documentNumberFilter
-	 *           the document number filter
-	 * @param administration
-	 *           the administration
-	 * @param fileTypeCodes
-	 *           the file type codes
-	 * @return the file
-	 */
-	File quickSearch(String documentNumberFilter, Administration administration, List<FileTypeCode> fileTypeCodes);
+        /**
+         * Find file item field value by field code.
+         *
+         * @param file the file
+         * @param fieldCode the field code
+         * @return the file item field value
+         */
+        FileFieldValue findFileFieldValueByFieldCode(File file, String fieldCode);
 
-	/**
-	 * Find by ref siat.
-	 *
-	 * @param refSiat
-	 *           the ref siat
-	 * @return the file
-	 */
-	File findByRefSiat(String refSiat);
+        /**
+         * Quick search.
+         *
+         * @param documentNumberFilter the document number filter
+         * @param administration the administration
+         * @param fileTypeCodes the file type codes
+         * @return the file
+         */
+        File quickSearch(String documentNumberFilter, Administration administration, List<FileTypeCode> fileTypeCodes);
 
-	/**
-	 * Find by num dossier guce.
-	 *
-	 * @param refGuce
-	 *           the ref guce
-	 * @return the file
-	 */
-	File findByNumDossierGuce(String refGuce);
+        /**
+         * Find by ref siat.
+         *
+         * @param refSiat the ref siat
+         * @return the file
+         */
+        File findByRefSiat(String refSiat);
 
-	/**
-	 * Find matching between manifest and prior notice.
-	 *
-	 * @param client
-	 *           the client
-	 * @param fileTypeCode
-	 *           the file type code
-	 * @param fileFieldValue1
-	 *           the file field value1
-	 * @param fileFieldValue2
-	 *           the file field value2
-	 * @param fileFieldCode1
-	 *           the file field code1
-	 * @param fileFieldCode2
-	 *           the file field code2
-	 * @return the file
-	 */
-	File findMatchingBetweenManifestAndPriorNotice(Company client, List<FileTypeCode> fileTypeCode,
-			FileFieldValue fileFieldValue1, FileFieldValue fileFieldValue2, String fileFieldCode1, String fileFieldCode2);
+        /**
+         * Find by num dossier guce.
+         *
+         * @param refGuce the ref guce
+         * @return the file
+         */
+        File findByNumDossierGuce(String refGuce);
 
-	/**
-	 * Find draft files by user.
-	 *
-	 * @param user
-	 *           the user
-	 * @return the list
-	 */
-	List<File> findDraftFilesByUsr(User user);
+        /**
+         * Find matching between manifest and prior notice.
+         *
+         * @param client the client
+         * @param fileTypeCode the file type code
+         * @param fileFieldValue1 the file field value1
+         * @param fileFieldValue2 the file field value2
+         * @param fileFieldCode1 the file field code1
+         * @param fileFieldCode2 the file field code2
+         * @return the file
+         */
+        File findMatchingBetweenManifestAndPriorNotice(Company client, List<FileTypeCode> fileTypeCode,
+                FileFieldValue fileFieldValue1, FileFieldValue fileFieldValue2, String fileFieldCode1, String fileFieldCode2);
 
+        /**
+         * Find draft files by user.
+         *
+         * @param user the user
+         * @return the list
+         */
+        List<File> findDraftFilesByUsr(User user);
 
-	/**
-	 * Find files by file type code.
-	 *
-	 * @param fileTypeCode
-	 *           the file type code
-	 * @return the list
-	 */
-	List<File> findFilesByFileTypeCode(FileTypeCode fileTypeCode);
+        /**
+         * Find files by file type code.
+         *
+         * @param fileTypeCode the file type code
+         * @return the list
+         */
+        List<File> findFilesByFileTypeCode(FileTypeCode fileTypeCode);
 
+        /**
+         * Find count by file type.
+         *
+         * @param fileTypeCode the file type code
+         * @return the integer
+         */
+        Long findCountByFileType(FileTypeCode fileTypeCode);
 
-	/**
-	 * Find count by file type.
-	 *
-	 * @param fileTypeCode
-	 *           the file type code
-	 * @return the integer
-	 */
-	Long findCountByFileType(FileTypeCode fileTypeCode);
+        /**
+         * Find by num contribuable.
+         *
+         * @param numContribuable the num contribuable
+         * @return the file
+         */
+        File findByNumContribuable(String numContribuable);
 
-	/**
-	 * Find by num contribuable.
-	 *
-	 * @param numContribuable
-	 *           the num contribuable
-	 * @return the file
-	 */
-	File findByNumContribuable(String numContribuable);
+        /**
+         * Find matching between fimex and prior notice.
+         *
+         * @param numContribuable the num contribuable
+         * @param fileTypeCodeList the file type code list
+         * @return the file
+         */
+        File findMatchingBetweenFimexAndPriorNotice(String numContribuable, List<FileTypeCode> fileTypeCodeList);
 
-	/**
-	 * Find matching between fimex and prior notice.
-	 *
-	 * @param numContribuable
-	 *           the num contribuable
-	 * @param fileTypeCodeList
-	 *           the file type code list
-	 * @return the file
-	 */
-	File findMatchingBetweenFimexAndPriorNotice(String numContribuable, List<FileTypeCode> fileTypeCodeList);
+        /**
+         * Find file if any, corresponding the following parameters.
+         *
+         * @param numeroDemande the NUMERO_DEMANDE from GUCE
+         * @param fileTypeGuce the file type code
+         * @return the file
+         */
+        File findByNumeroDemandeAndFileTypeGuce(String numeroDemande, String fileTypeGuce);
 
-
-	/**
-	 * Update specific column.
-	 *
-	 * @param paramsMap
-	 *           the params map
-	 * @param file
-	 *           the file
-	 */
-	void updateSpecificColumn(Map<String, ?> paramsMap, File file);
+        /**
+         * Update specific column.
+         *
+         * @param paramsMap the params map
+         * @param file the file
+         */
+        void updateSpecificColumn(Map<String, ?> paramsMap, File file);
 }
--- src/main/java/org/guce/siat/common/service/FileService.java
+++ src/main/java/org/guce/siat/common/service/FileService.java
@@ -10,105 +10,92 @@ import org.guce.siat.common.model.FileFieldValue;
 import org.guce.siat.common.model.User;
 import org.guce.siat.common.utils.enums.FileTypeCode;
 
-
-
 /**
  * The Interface FileService.
  */
-public interface FileService extends AbstractService<File>
-{
-	/**
-	 * Find file item field value by field code.
-	 *
-	 * @param file
-	 *           the file
-	 * @param fieldCode
-	 *           the field code
-	 * @return the file item field value
-	 */
-	FileFieldValue findFileFieldValueByFieldCode(File file, String fieldCode);
+public interface FileService extends AbstractService<File> {
 
-	/**
-	 * Quick search.
-	 *
-	 * @param documentNumberFilter
-	 *           the document number filter
-	 * @param administration
-	 *           the administration
-	 * @param loggedUser
-	 *           the logged user
-	 * @return the file
-	 */
-	File quickSearch(final String documentNumberFilter, final Administration administration, User loggedUser);
+        /**
+         * Find file item field value by field code.
+         *
+         * @param file the file
+         * @param fieldCode the field code
+         * @return the file item field value
+         */
+        FileFieldValue findFileFieldValueByFieldCode(File file, String fieldCode);
 
-	/**
-	 * Find matching between manifest and prior notice.
-	 *
-	 * @param client
-	 *           the client
-	 * @param fileTypeCodes
-	 *           the file type codes
-	 * @param fileFieldValue1
-	 *           the file field value1
-	 * @param fileFieldValue2
-	 *           the file field value2
-	 * @param fileFieldCode1
-	 *           the file field code1
-	 * @param fileFieldCode2
-	 *           the file field code2
-	 * @return the file
-	 */
-	File findMatchingBetweenManifestAndPriorNotice(final Company client, List<FileTypeCode> fileTypeCodes,
-			final FileFieldValue fileFieldValue1, final FileFieldValue fileFieldValue2, String fileFieldCode1, String fileFieldCode2);
+        /**
+         * Quick search.
+         *
+         * @param documentNumberFilter the document number filter
+         * @param administration the administration
+         * @param loggedUser the logged user
+         * @return the file
+         */
+        File quickSearch(final String documentNumberFilter, final Administration administration, User loggedUser);
 
-	/**
-	 * Find draft files by usr.
-	 *
-	 * @param user
-	 *           the user
-	 * @return the list
-	 */
-	List<File> findDraftFilesByUsr(User user);
+        /**
+         * Find matching between manifest and prior notice.
+         *
+         * @param client the client
+         * @param fileTypeCodes the file type codes
+         * @param fileFieldValue1 the file field value1
+         * @param fileFieldValue2 the file field value2
+         * @param fileFieldCode1 the file field code1
+         * @param fileFieldCode2 the file field code2
+         * @return the file
+         */
+        File findMatchingBetweenManifestAndPriorNotice(final Company client, List<FileTypeCode> fileTypeCodes,
+                final FileFieldValue fileFieldValue1, final FileFieldValue fileFieldValue2, String fileFieldCode1, String fileFieldCode2);
 
+        /**
+         * Find draft files by usr.
+         *
+         * @param user the user
+         * @return the list
+         */
+        List<File> findDraftFilesByUsr(User user);
 
-	/**
-	 * Find fimex filesby bureaux.
-	 *
-	 * @param fileTypeCode
-	 *           the file type code
-	 * @return the list
-	 */
-	List<File> findFilesByFileTypeCode(FileTypeCode fileTypeCode);
+        /**
+         * Find fimex filesby bureaux.
+         *
+         * @param fileTypeCode the file type code
+         * @return the list
+         */
+        List<File> findFilesByFileTypeCode(FileTypeCode fileTypeCode);
 
-	/**
-	 * Find count by file type.
-	 *
-	 * @param fileTypeCode
-	 *           the file type code
-	 * @return the integer
-	 */
-	Long findCountByFileType(FileTypeCode fileTypeCode);
+        /**
+         * Find count by file type.
+         *
+         * @param fileTypeCode the file type code
+         * @return the integer
+         */
+        Long findCountByFileType(FileTypeCode fileTypeCode);
 
-	/**
-	 * Find matching between fimex and prior notice.
-	 *
-	 * @param numContribuable
-	 *           the num contribuable
-	 * @param fileTypeCodeList
-	 *           the file type code list
-	 * @return the file
-	 */
-	File findMatchingBetweenFimexAndPriorNotice(String numContribuable, List<FileTypeCode> fileTypeCodeList);
+        /**
+         * Find matching between fimex and prior notice.
+         *
+         * @param numContribuable the num contribuable
+         * @param fileTypeCodeList the file type code list
+         * @return the file
+         */
+        File findMatchingBetweenFimexAndPriorNotice(String numContribuable, List<FileTypeCode> fileTypeCodeList);
 
+        /**
+         * Find file if any, corresponding the following parameters.
+         *
+         * @param numeroDemande the NUMERO_DEMANDE from GUCE
+         * @param fileTypeGuce the file type code
+         * @return the file
+         */
+        File findByNumeroDemandeAndFileTypeGuce(String numeroDemande, String fileTypeGuce);
 
-	/**
-	 * Update specific column.
-	 *
-	 * @param paramsMap
-	 *           the params map
-	 * @param file
-	 *           the file
-	 */
-	void updateSpecificColumn(final Map<String, ?> paramsMap, final File file);
+        /**
+         * Update specific column.
+         *
+         * @param paramsMap the params map
+         * @param file the file
+         */
+        void updateSpecificColumn(final Map<String, ?> paramsMap, final File file);
 
 }
