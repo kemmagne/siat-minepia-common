bbae79701233bf6add3e949e63ddc24f6245ea10
Hyacinthe TADZOTSA <romaulad.tadzotsa@guichetunique.org>
2018-02-28 08:49:31 +0100

    ajout d'une méthode pour la récupération des flows

--- src/main/java/org/guce/siat/common/service/FlowService.java
+++ src/main/java/org/guce/siat/common/service/FlowService.java
@@ -8,118 +8,111 @@ import org.guce.siat.common.model.Flow;
 import org.guce.siat.common.model.Step;
 import org.guce.siat.common.utils.enums.FileTypeCode;
 
-
-
 /**
  * The Interface FlowService.
  */
-public interface FlowService extends AbstractService<Flow>
-{
+public interface FlowService extends AbstractService<Flow> {
 
-	/**
-	 * Find flows by from step and file type.
-	 *
-	 * @param step
-	 *           the step
-	 * @param fileType
-	 *           the file type
-	 * @return the list
-	 */
-	List<Flow> findFlowsByFromStepAndFileType(Step step, FileType fileType);
+    /**
+     * Find flows by from step and file type.
+     *
+     * @param step the step
+     * @param fileType the file type
+     * @return the list
+     */
+    List<Flow> findFlowsByFromStepAndFileType(Step step, FileType fileType);
 
-	/**
-	 * Find by steps i ds.
-	 *
-	 * @param idsSteps
-	 *           the ids steps
-	 * @return the list
-	 */
-	List<Flow> findByStepsIDs(List<Long> idsSteps);
+    /**
+     * Find flows by from step and file type by using the file_type_flow table
+     * other than the flow table like usually
+     *
+     * @param step
+     * @param fileType
+     * @return
+     */
+    List<Flow> findFlowsByFromStepAndFileType2(Step step, FileType fileType);
 
-	/**
-	 * Find flow by label.
-	 *
-	 * @param flowLabel
-	 *           the flow label
-	 * @param source
-	 *           the source
-	 * @param target
-	 *           the target
-	 * @param local
-	 *           the local
-	 * @return the flow
-	 */
-	Flow findFlowByLabelSourceAndTarget(String flowLabel, String source, String target, String local);
+    /**
+     * Find by steps i ds.
+     *
+     * @param idsSteps the ids steps
+     * @return the list
+     */
+    List<Flow> findByStepsIDs(List<Long> idsSteps);
 
-	/**
-	 * Find flow by current step.
-	 *
-	 * @param step
-	 *           the step
-	 * @return the flow
-	 */
-	Flow findFlowByCurrentStep(Step step);
+    /**
+     * Find flow by label.
+     *
+     * @param flowLabel the flow label
+     * @param source the source
+     * @param target the target
+     * @param local the local
+     * @return the flow
+     */
+    Flow findFlowByLabelSourceAndTarget(String flowLabel, String source, String target, String local);
 
+    /**
+     * Find flow by current step.
+     *
+     * @param step the step
+     * @return the flow
+     */
+    Flow findFlowByCurrentStep(Step step);
 
-	/**
-	 * Find flow by sent file item.
-	 *
-	 * @param fileItem
-	 *           the file item
-	 * @return the flow
-	 */
-	Flow findFlowBySentFileItem(FileItem fileItem);
+    /**
+     * Find flow by sent file item.
+     *
+     * @param fileItem the file item
+     * @return the flow
+     */
+    Flow findFlowBySentFileItem(FileItem fileItem);
 
+    /**
+     * Find flow by code.
+     *
+     * @param flowCode the flow code
+     * @return the flow
+     */
+    Flow findFlowByCode(String flowCode);
 
-	/**
-	 * Find flow by code.
-	 *
-	 * @param flowCode
-	 *           the flow code
-	 * @return the flow
-	 */
-	Flow findFlowByCode(String flowCode);
+    /**
+     * Find flow list by flow code list.
+     *
+     * @param flowCodeList the flow code list
+     * @return the list
+     */
+    List<Flow> findFlowListByFlowCodeList(List<String> flowCodeList);
 
-	/**
-	 * Find flow list by flow code list.
-	 *
-	 * @param flowCodeList
-	 *           the flow code list
-	 * @return the list
-	 */
-	List<Flow> findFlowListByFlowCodeList(List<String> flowCodeList);
+    /**
+     * Find final decisions.
+     *
+     * @return the list
+     */
+    List<Flow> findFinalDecisions();
 
-	/**
-	 * Find final decisions.
-	 *
-	 * @return the list
-	 */
-	List<Flow> findFinalDecisions();
-	
-	/**
-	 * Find flow by file type.
-	 *
-	 * @param coGco
-	 *           the co gco
-	 * @return the list
-	 */
-	List<Flow> findFlowByFileType(FileTypeCode coGco);
-	
-	/**
-	 * Find flow by file types.
-	 *
-	 * @param fileTypeCodes
-	 *           the file type codes
-	 * @return the list 
-	 */
-	List<Flow> findFlowByFileTypes(List<FileTypeCode> fileTypeCodes);
-	
-	/**
-	 * Find by to step.
-	 *
-	 * @param step the step
-	 * @return the flow
-	 */
-	Flow findByToStep(Step step);
+    /**
+     * Find flow by file type.
+     *
+     * @param coGco the co gco
+     * @return the list
+     */
+    List<Flow> findFlowByFileType(FileTypeCode coGco);
+
+    /**
+     * Find flow by file types.
+     *
+     * @param fileTypeCodes the file type codes
+     * @return the list
+     */
+    List<Flow> findFlowByFileTypes(List<FileTypeCode> fileTypeCodes);
+
+    /**
+     * Find by to step.
+     *
+     * @param step the step
+     * @return the flow
+     */
+    Flow findByToStep(Step step);
 
 }
+
--- src/main/java/org/guce/siat/common/service/impl/FlowServiceImpl.java
+++ src/main/java/org/guce/siat/common/service/impl/FlowServiceImpl.java
@@ -17,169 +17,161 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
 /**
  * The Class FlowServiceImpl.
  */
 @Service("flowService")
 @Transactional(readOnly = true)
-public class FlowServiceImpl extends AbstractServiceImpl<Flow> implements FlowService
-{
+public class FlowServiceImpl extends AbstractServiceImpl<Flow> implements FlowService {
 
-	/** The flow dao. */
-	@Autowired
-	private FlowDao flowDao;
+    /**
+     * The flow dao.
+     */
+    @Autowired
+    private FlowDao flowDao;
 
-	/**
-	 * Instantiates a new flow service impl.
-	 */
-	public FlowServiceImpl()
-	{
-		super();
-	}
+    /**
+     * Instantiates a new flow service impl.
+     */
+    public FlowServiceImpl() {
+        super();
+    }
 
-	/*
+    /*
 	 * (non-Javadoc)
 	 *
 	 * @see org.guce.siat.common.service.impl.AbstractServiceImpl#getJpaDao()
-	 */
-	@Override
-	public AbstractJpaDao<Flow> getJpaDao()
-	{
-		return flowDao;
-	}
+     */
+    @Override
+    public AbstractJpaDao<Flow> getJpaDao() {
+        return flowDao;
+    }
 
-	/*
+    /*
 	 * (non-Javadoc)
 	 *
 	 * @see org.guce.siat.common.service.impl.AbstractServiceImpl#setJpaDao(org.guce.siat.common.dao.AbstractJpaDao)
-	 */
-	@Override
-	public void setJpaDao(final AbstractJpaDao<Flow> jpaDao)
-	{
-		this.flowDao = (FlowDao) jpaDao;
-	}
+     */
+    @Override
+    public void setJpaDao(final AbstractJpaDao<Flow> jpaDao) {
+        this.flowDao = (FlowDao) jpaDao;
+    }
 
 
-	/*
+    /*
 	 * (non-Javadoc)
 	 *
 	 * @see org.guce.siat.common.service.FlowService#findFlowsByFromStepAndFileType(org.guce.siat.common.model.Step,
 	 * org.guce.siat.common.model.FileType)
-	 */
-	@Override
-	public List<Flow> findFlowsByFromStepAndFileType(final Step step, final FileType fileType)
-	{
-		return flowDao.findFlowsByFromStepAndFileType(step, fileType);
-	}
+     */
+    @Override
+    public List<Flow> findFlowsByFromStepAndFileType(final Step step, final FileType fileType) {
+        return flowDao.findFlowsByFromStepAndFileType(step, fileType);
+    }
 
-	/*
+    @Override
+    public List<Flow> findFlowsByFromStepAndFileType2(final Step step, final FileType fileType) {
+        return flowDao.findFlowsByFromStepAndFileType2(step, fileType);
+    }
+
+    /*
 	 * (non-Javadoc)
 	 *
 	 * @see org.guce.siat.core.ct.service.FlowService#findByStepsIDs(java.util.List)
-	 */
-	@Override
-	public List<Flow> findByStepsIDs(final List<Long> idsSteps)
-	{
-		return flowDao.findByStepsIDs(idsSteps);
-	}
+     */
+    @Override
+    public List<Flow> findByStepsIDs(final List<Long> idsSteps) {
+        return flowDao.findByStepsIDs(idsSteps);
+    }
 
-	/*
+    /*
 	 * (non-Javadoc)
 	 *
 	 * @see org.guce.siat.core.ct.service.FlowService#findFlowByLabel(java.lang.String)
-	 */
-	@Override
-	public Flow findFlowByLabelSourceAndTarget(final String flowLabel, final String source, final String target, final String local)
-	{
-		return flowDao.findFlowByLabelSourceAndTarget(flowLabel, source, target, local);
-	}
+     */
+    @Override
+    public Flow findFlowByLabelSourceAndTarget(final String flowLabel, final String source, final String target, final String local) {
+        return flowDao.findFlowByLabelSourceAndTarget(flowLabel, source, target, local);
+    }
 
-	/*
+    /*
 	 * (non-Javadoc)
 	 *
 	 * @see org.guce.siat.core.ct.service.FlowService#findFlowByCurrentStep(org.guce.siat.core.ct.model.Step)
-	 */
-	@Override
-	public Flow findFlowByCurrentStep(final Step step)
-	{
-		return flowDao.findFlowByCurrentStep(step);
-	}
+     */
+    @Override
+    public Flow findFlowByCurrentStep(final Step step) {
+        return flowDao.findFlowByCurrentStep(step);
+    }
 
-	/*
+    /*
 	 * (non-Javadoc)
 	 *
 	 * @see org.guce.siat.common.service.FlowService#findFlowBySentFileItem(org.guce.siat.common.model.FileItem)
-	 */
-	@Override
-	public Flow findFlowBySentFileItem(final FileItem fileItem)
-	{
-		return flowDao.findFlowBySentFileItem(fileItem);
-	}
+     */
+    @Override
+    public Flow findFlowBySentFileItem(final FileItem fileItem) {
+        return flowDao.findFlowBySentFileItem(fileItem);
+    }
 
-	/*
+    /*
 	 * (non-Javadoc)
 	 *
 	 * @see org.guce.siat.common.service.FlowService#findFlowByCode(java.lang.String)
-	 */
-	@Override
-	public Flow findFlowByCode(final String flowCode)
-	{
-		return flowDao.findFlowByCode(flowCode);
-	}
+     */
+    @Override
+    public Flow findFlowByCode(final String flowCode) {
+        return flowDao.findFlowByCode(flowCode);
+    }
 
-	/*
+    /*
 	 * (non-Javadoc)
 	 *
 	 * @see org.guce.siat.common.service.FlowService#findFlowListByFlowCodeList(java.util.List)
-	 */
-	@Override
-	public List<Flow> findFlowListByFlowCodeList(final List<String> flowCodeList)
-	{
-		return flowDao.findFlowListByFlowCodeList(flowCodeList);
-	}
+     */
+    @Override
+    public List<Flow> findFlowListByFlowCodeList(final List<String> flowCodeList) {
+        return flowDao.findFlowListByFlowCodeList(flowCodeList);
+    }
 
-	/*
+    /*
 	 * (non-Javadoc)
 	 *
 	 * @see org.guce.siat.common.service.FlowService#findFinalDecisions()
-	 */
-	@Override
-	public List<Flow> findFinalDecisions()
-	{
-		return flowDao.findFinalDecisions();
-	}
+     */
+    @Override
+    public List<Flow> findFinalDecisions() {
+        return flowDao.findFinalDecisions();
+    }
 
-	/*
+    /*
 	 * (non-Javadoc)
 	 *
 	 * @see org.guce.siat.common.service.FlowService#findFlowByFileType(org.guce.siat.common.utils.enums.FileTypeCode)
-	 */
-	@Override
-	public List<Flow> findFlowByFileType(final FileTypeCode fileType)
-	{
-		return flowDao.findFlowByFileType(fileType);
-	}
+     */
+    @Override
+    public List<Flow> findFlowByFileType(final FileTypeCode fileType) {
+        return flowDao.findFlowByFileType(fileType);
+    }
 
-	/*
+    /*
 	 * (non-Javadoc)
 	 *
 	 * @see org.guce.siat.common.service.FlowService#findFlowByFileTypes(java.util.List)
-	 */
-	@Override
-	public List<Flow> findFlowByFileTypes(final List<FileTypeCode> fileTypeCodes)
-	{
-		return flowDao.findFlowByFileTypes(fileTypeCodes);
-	}
+     */
+    @Override
+    public List<Flow> findFlowByFileTypes(final List<FileTypeCode> fileTypeCodes) {
+        return flowDao.findFlowByFileTypes(fileTypeCodes);
+    }
 
-	/*
+    /*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see org.guce.siat.common.service.FlowService#findByToStep(org.guce.siat.common.model.Step)
-	 */
-	@Override
-	public Flow findByToStep(final Step step)
-	{
-		return flowDao.findByToStep(step);
-	}
+     */
+    @Override
+    public Flow findByToStep(final Step step) {
+        return flowDao.findByToStep(step);
+    }
 }
+
