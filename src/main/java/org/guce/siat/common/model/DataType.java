c35cc957683862739c4ba67866aa6c2b75580447
Hyacinthe TADZOTSA <romaulad.tadzotsa@guichetunique.org>
2018-04-17 10:22:11 +0100

    Ajout de nouvelles procédures, flows et steps pour phyto Mise à jour xsd

--- src/main/java/org/guce/siat/common/model/DataType.java
+++ src/main/java/org/guce/siat/common/model/DataType.java
@@ -14,258 +14,256 @@ import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
 
-
 /**
  * The Class DataType.
  */
 @Entity
 @Table(name = "DATA_TYPE")
 @XmlRootElement
-public class DataType extends AbstractModel implements Serializable
-{
+public class DataType extends AbstractModel implements Serializable {
 
-	/** The Constant serialVersionUID. */
-	private static final long serialVersionUID = 1L;
+    /**
+     * The Constant serialVersionUID.
+     */
+    private static final long serialVersionUID = 1L;
 
-	/** The id. */
-	@Id
-	@Basic(optional = false)
-	@Column(name = "ID")
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "DATA_TYPE_SEQ")
-	@SequenceGenerator(name = "DATA_TYPE_SEQ", sequenceName = "DATA_TYPE_SEQ", allocationSize = 1)
-	private Long id;
+    /**
+     * The id.
+     */
+    @Id
+    @Basic(optional = false)
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "DATA_TYPE_SEQ")
+    @SequenceGenerator(name = "DATA_TYPE_SEQ", sequenceName = "DATA_TYPE_SEQ", allocationSize = 1)
+    private Long id;
 
-	/** The label. */
-	@Column(name = "LABEL", length = 150)
-	private String label;
+    /**
+     * The label.
+     */
+    @Column(name = "LABEL", length = 150)
+    private String label;
 
-	/** The label. */
-	@Column(name = "LABEL_EN", length = 150)
-	private String labelEn;
-	/** The type. */
-	@Column(name = "TYPE", length = 100)
-	private String type;
+    /**
+     * The label.
+     */
+    @Column(name = "LABEL_EN", length = 150)
+    private String labelEn;
+    /**
+     * The type.
+     */
+    @Column(name = "TYPE", length = 100)
+    private String type;
 
-	/** The required. */
-	@Column(name = "REQUIRED")
-	private Boolean required;
+    /**
+     * The required.
+     */
+    @Column(name = "REQUIRED")
+    private Boolean required;
 
-	/** The object type. */
-	@Column(name = "OBJECT_TYPE")
-	private String objectType;
+    /**
+     * The object type.
+     */
+    @Column(name = "OBJECT_TYPE")
+    private String objectType;
 
+    /**
+     * The flow.
+     */
+    @JoinColumn(name = "FLOW_ID", referencedColumnName = "ID")
+    @ManyToOne
+    private Flow flow;
 
-	/** The flow. */
-	@JoinColumn(name = "FLOW_ID", referencedColumnName = "ID")
-	@ManyToOne
-	private Flow flow;
+    /**
+     * Others props of data type.
+     */
+    @Column(name = "PROPS", length = 1000)
+    private String props;
 
-	/**
-	 * Instantiates a new data type.
-	 */
-	public DataType()
-	{
-	}
+    /**
+     * Instantiates a new data type.
+     */
+    public DataType() {
+    }
 
-	/**
-	 * Gets the id.
-	 *
-	 * @return the id
-	 */
-	@Override
-	public Long getId()
-	{
-		return id;
-	}
+    /**
+     * Gets the id.
+     *
+     * @return the id
+     */
+    @Override
+    public Long getId() {
+        return id;
+    }
 
-	/**
-	 * Sets the id.
-	 *
-	 * @param id
-	 *           the id to set
-	 */
-	@Override
-	public void setId(final Long id)
-	{
-		this.id = id;
-	}
+    /**
+     * Sets the id.
+     *
+     * @param id the id to set
+     */
+    @Override
+    public void setId(final Long id) {
+        this.id = id;
+    }
 
-	/**
-	 * Gets the label.
-	 *
-	 * @return the label
-	 */
-	public String getLabel()
-	{
-		return label;
-	}
+    /**
+     * Gets the label.
+     *
+     * @return the label
+     */
+    public String getLabel() {
+        return label;
+    }
 
-	/**
-	 * Sets the label.
-	 *
-	 * @param label
-	 *           the label to set
-	 */
-	public void setLabel(final String label)
-	{
-		this.label = label;
-	}
+    /**
+     * Sets the label.
+     *
+     * @param label the label to set
+     */
+    public void setLabel(final String label) {
+        this.label = label;
+    }
 
+    /**
+     * Gets the flow.
+     *
+     * @return the flow
+     */
+    public Flow getFlow() {
+        return flow;
+    }
 
-	/**
-	 * Gets the flow.
-	 *
-	 * @return the flow
-	 */
-	public Flow getFlow()
-	{
-		return flow;
-	}
+    /**
+     * Sets the flow.
+     *
+     * @param flow the flow to set
+     */
+    public void setFlow(final Flow flow) {
+        this.flow = flow;
+    }
 
-	/**
-	 * Sets the flow.
-	 *
-	 * @param flow
-	 *           the flow to set
-	 */
-	public void setFlow(final Flow flow)
-	{
-		this.flow = flow;
-	}
+    /**
+     * Gets the type.
+     *
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
 
-	/**
-	 * Gets the type.
-	 *
-	 * @return the type
-	 */
-	public String getType()
-	{
-		return type;
-	}
+    /**
+     * Sets the type.
+     *
+     * @param type the type to set
+     */
+    public void setType(final String type) {
+        this.type = type;
+    }
 
-	/**
-	 * Sets the type.
-	 *
-	 * @param type
-	 *           the type to set
-	 */
-	public void setType(final String type)
-	{
-		this.type = type;
-	}
+    /**
+     * Gets the required.
+     *
+     * @return the required
+     */
+    public Boolean getRequired() {
+        return required;
+    }
 
-	/**
-	 * Gets the required.
-	 *
-	 * @return the required
-	 */
-	public Boolean getRequired()
-	{
-		return required;
-	}
+    /**
+     * Sets the required.
+     *
+     * @param required the required to set
+     */
+    public void setRequired(final Boolean required) {
+        this.required = required;
+    }
 
-	/**
-	 * Sets the required.
-	 *
-	 * @param required
-	 *           the required to set
-	 */
-	public void setRequired(final Boolean required)
-	{
-		this.required = required;
-	}
+    /**
+     * Gets the label en.
+     *
+     * @return the label en
+     */
+    public String getLabelEn() {
+        return labelEn;
+    }
 
+    /**
+     * Sets the label en.
+     *
+     * @param labelEn the new label en
+     */
+    public void setLabelEn(final String labelEn) {
+        this.labelEn = labelEn;
+    }
 
-	/**
-	 * Gets the label en.
-	 *
-	 * @return the label en
-	 */
-	public String getLabelEn()
-	{
-		return labelEn;
-	}
+    public String getProps() {
+        return props;
+    }
 
-	/**
-	 * Sets the label en.
-	 *
-	 * @param labelEn
-	 *           the new label en
-	 */
-	public void setLabelEn(final String labelEn)
-	{
-		this.labelEn = labelEn;
-	}
+    public void setProps(String props) {
+        this.props = props;
+    }
 
-	/*
+    /*
 	 * (non-Javadoc)
 	 *
 	 * @see java.lang.Object#hashCode()
-	 */
-	@Override
-	public int hashCode()
-	{
-		int hash = 0;
-		hash += (id != null ? id.hashCode() : 0);
-		return hash;
-	}
+     */
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
 
-	/*
+    /*
 	 * (non-Javadoc)
 	 *
 	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(final Object object)
-	{
-		if (!(object instanceof DataType))
-		{
-			return false;
-		}
-		final DataType other = (DataType) object;
-		if ((this.getId() == null && other.getId() != null) || (this.getId() != null && !this.getId().equals(other.getId())))
-		{
-			return false;
-		}
-		return true;
-	}
+     */
+    @Override
+    public boolean equals(final Object object) {
+        if (!(object instanceof DataType)) {
+            return false;
+        }
+        final DataType other = (DataType) object;
+        if ((this.getId() == null && other.getId() != null) || (this.getId() != null && !this.getId().equals(other.getId()))) {
+            return false;
+        }
+        return true;
+    }
 
-	/**
-	 * Gets the object type.
-	 *
-	 * @return the object type
-	 */
-	public String getObjectType()
-	{
-		return objectType;
-	}
+    /**
+     * Gets the object type.
+     *
+     * @return the object type
+     */
+    public String getObjectType() {
+        return objectType;
+    }
 
-	/**
-	 * Sets the object type.
-	 *
-	 * @param objectType
-	 *           the new object type
-	 */
-	public void setObjectType(final String objectType)
-	{
-		this.objectType = objectType;
-	}
+    /**
+     * Sets the object type.
+     *
+     * @param objectType the new object type
+     */
+    public void setObjectType(final String objectType) {
+        this.objectType = objectType;
+    }
 
-	/*
+    /*
 	 * (non-Javadoc)
 	 *
 	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString()
-	{
-		final StringBuilder builder = new StringBuilder();
-		builder.append("DataType [id=");
-		builder.append(id);
-		builder.append(", label=");
-		builder.append(label);
-		builder.append("]");
-		return builder.toString();
-	}
+     */
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append("DataType [id=");
+        builder.append(id);
+        builder.append(", label=");
+        builder.append(label);
+        builder.append("]");
+        return builder.toString();
+    }
 
 }
--- src/test/java/org/guce/siat/common/utils/QRCodeUtilsTest.java
+++ src/test/java/org/guce/siat/common/utils/QRCodeUtilsTest.java
@@ -2,8 +2,8 @@ package org.guce.siat.common.utils;
 
 import com.google.zxing.WriterException;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import org.guce.siat.common.model.File;
-import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -13,7 +13,7 @@ import org.junit.Test;
  */
 public class QRCodeUtilsTest {
 
-//    @Ignore
+    @Ignore
     @Test
     public void test() throws WriterException, IOException, NoSuchMethodException {
         File siatFile = new File();
@@ -24,4 +24,11 @@ public class QRCodeUtilsTest {
 //        Assert.assertTrue();
     }
 
+    @Ignore
+    @Test
+    public void test1() {
+        System.out.println(new DecimalFormat("CT" + "000000").format(985));
+    }
+
 }
+
