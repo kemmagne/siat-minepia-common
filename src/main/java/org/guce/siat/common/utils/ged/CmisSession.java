8cf92350ab5cdd571cfc17dc42d65a43807c493a
tadzotsa <tadzotsa@LT-DOP-SDAb.guce.local>
2019-02-07 16:30:46 +0100

    updates

--- src/main/java/org/guce/siat/common/utils/ged/CmisSession.java
+++ src/main/java/org/guce/siat/common/utils/ged/CmisSession.java
@@ -13,55 +13,56 @@ import org.slf4j.LoggerFactory;
  */
 public final class CmisSession {
 
-	private static final Logger LOG = LoggerFactory.getLogger(CmisUtils.class);
+    private static final Logger LOG = LoggerFactory.getLogger(CmisSession.class);
 
-	/**
-	 * The instance.
-	 */
-	private static Session instance;
+    /**
+     * The instance.
+     */
+    private static Session instance;
 
-	/**
-	 * Instantiates a new cimis session. Signleton must hide the public
-	 * Constructor
-	 */
-	private CmisSession() {
-		final CmisClient cmisClient = new CmisClient();
-		final Properties param = getConnectionParameters();
-		instance = cmisClient.getSession(param.getProperty("connectionName"), param.getProperty("userNameRepo"),
-				param.getProperty("pwdRepo"), param.getProperty("ipRepo") + param.getProperty("urlAtomRepo"),
-				param.getProperty("idRepo"));
-	}
+    /**
+     * Instantiates a new cimis session. Signleton must hide the public
+     * Constructor
+     */
+    private CmisSession() {
+        final CmisClient cmisClient = new CmisClient();
+        final Properties param = getConnectionParameters();
+        instance = cmisClient.getSession(param.getProperty("connectionName"), param.getProperty("userNameRepo"),
+                param.getProperty("pwdRepo"), param.getProperty("ipRepo") + param.getProperty("urlAtomRepo"),
+                param.getProperty("idRepo"));
+    }
 
-	/**
-	 * Gets the single instance of CimisSession.
-	 *
-	 * @return single instance of CimisSession
-	 */
-	public static synchronized Session getInstance() {
-		if (instance == null) {
-			new CmisSession();
-		}
-		return instance;
-	}
+    /**
+     * Gets the single instance of CimisSession.
+     *
+     * @return single instance of CimisSession
+     */
+    public static synchronized Session getInstance() {
+        if (instance == null) {
+            new CmisSession();
+        }
+        return instance;
+    }
 
-	private Properties getConnectionParameters() {
+    private Properties getConnectionParameters() {
 
-		final Properties prop = new Properties();
-		final String filename = "global-config.properties";
-		try (InputStream input = CmisSession.class.getClassLoader().getResourceAsStream(filename)) {
+        final Properties prop = new Properties();
+        final String filename = "global-config.properties";
+        try (InputStream input = CmisSession.class.getClassLoader().getResourceAsStream(filename)) {
 
-			if (input == null) {
-				LOG.error("Sorry, unable to find {}", filename);
-				return null;
-			}
+            if (input == null) {
+                LOG.error("Sorry, unable to find {}", filename);
+                return null;
+            }
 
-			prop.load(input);
+            prop.load(input);
 
-		} catch (final IOException ex) {
-			LOG.error(ex.getMessage(), ex);
-		}
+        } catch (final IOException ex) {
+            LOG.error(ex.getMessage(), ex);
+        }
 
-		return prop;
+        return prop;
 
-	}
+    }
 }
+
