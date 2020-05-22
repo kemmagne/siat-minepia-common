4f3ce0d8e37475d6c87ae7c99e7abb1813792c49
ht <ht@DESKTOP-HT>
2020-05-22 14:02:56 +0100

    updates

--- src/main/java/org/guce/siat/common/model/UserAuthorityFileType.java
+++ src/main/java/org/guce/siat/common/model/UserAuthorityFileType.java
@@ -1,7 +1,6 @@
 package org.guce.siat.common.model;
 
 import java.io.Serializable;
-
 import javax.persistence.AssociationOverride;
 import javax.persistence.AssociationOverrides;
 import javax.persistence.EmbeddedId;
@@ -11,93 +10,83 @@ import javax.persistence.Table;
 import javax.persistence.Transient;
 import javax.xml.bind.annotation.XmlRootElement;
 
-import org.guce.siat.common.model.AbstractModel;
-import org.guce.siat.common.model.UserAuthority;
-
-
 /**
  * The Class UserAuthorityFileType.
  */
 @Entity
 @Table(name = "USER_AUTHORIRTY_FILE_TYPE")
 @XmlRootElement
-@AssociationOverrides(
-{ @AssociationOverride(name = "primaryKey.userAuthority", joinColumns = @JoinColumn(name = "USER_AUTHORITY_ID")),
-		@AssociationOverride(name = "primaryKey.fileType", joinColumns = @JoinColumn(name = "FILE_TYPE_ID")) })
-public class UserAuthorityFileType extends AbstractModel implements Serializable
-{
+@AssociationOverrides({
+    @AssociationOverride(name = "primaryKey.userAuthority", joinColumns = @JoinColumn(name = "USER_AUTHORITY_ID")),
+    @AssociationOverride(name = "primaryKey.fileType", joinColumns = @JoinColumn(name = "FILE_TYPE_ID"))}
+)
+public class UserAuthorityFileType extends AbstractModel implements Serializable {
 
-	/** The Constant serialVersionUID. */
-	private static final long serialVersionUID = -9098449738561410816L;
+    /**
+     * The Constant serialVersionUID.
+     */
+    private static final long serialVersionUID = -9098449738561410816L;
 
-	/** The primary key. */
-	@EmbeddedId
-	private UserAuthorityFileTypeId primaryKey = new UserAuthorityFileTypeId();
+    /**
+     * The primary key.
+     */
+    @EmbeddedId
+    private UserAuthorityFileTypeId primaryKey = new UserAuthorityFileTypeId();
 
+    /**
+     * Gets the primary key.
+     *
+     * @return the primary key
+     */
+    public UserAuthorityFileTypeId getPrimaryKey() {
+        return primaryKey;
+    }
 
-	/**
-	 * Gets the primary key.
-	 * 
-	 * @return the primary key
-	 */
-	public UserAuthorityFileTypeId getPrimaryKey()
-	{
-		return primaryKey;
-	}
+    /**
+     * Sets the primary key.
+     *
+     * @param primaryKey the new primary key
+     */
+    public void setPrimaryKey(final UserAuthorityFileTypeId primaryKey) {
+        this.primaryKey = primaryKey;
+    }
 
-	/**
-	 * Sets the primary key.
-	 * 
-	 * @param primaryKey
-	 *           the new primary key
-	 */
-	public void setPrimaryKey(final UserAuthorityFileTypeId primaryKey)
-	{
-		this.primaryKey = primaryKey;
-	}
+    /**
+     * Gets the user authority.
+     *
+     * @return the user authority
+     */
+    @Transient
+    public UserAuthority getUserAuthority() {
+        return primaryKey.getUserAuthority();
+    }
 
-	/**
-	 * Gets the user authority.
-	 * 
-	 * @return the user authority
-	 */
-	@Transient
-	public UserAuthority getUserAuthority()
-	{
-		return primaryKey.getUserAuthority();
-	}
+    /**
+     * Gets the file type.
+     *
+     * @return the file type
+     */
+    @Transient
+    public FileType getFileType() {
+        return primaryKey.getFileType();
+    }
 
-	/**
-	 * Gets the file type.
-	 * 
-	 * @return the file type
-	 */
-	@Transient
-	public FileType getFileType()
-	{
-		return primaryKey.getFileType();
-	}
+    /**
+     * Sets the user authority.
+     *
+     * @param userAuthority the new user authority
+     */
+    public void setUserAuthority(final UserAuthority userAuthority) {
+        primaryKey.setUserAuthority(userAuthority);
+    }
 
-	/**
-	 * Sets the user authority.
-	 * 
-	 * @param userAuthority
-	 *           the new user authority
-	 */
-	public void setUserAuthority(final UserAuthority userAuthority)
-	{
-		primaryKey.setUserAuthority(userAuthority);
-	}
-
-	/**
-	 * Sets the file type.
-	 * 
-	 * @param fileType
-	 *           the new file type
-	 */
-	public void setFileType(final FileType fileType)
-	{
-		primaryKey.setFileType(fileType);
-	}
+    /**
+     * Sets the file type.
+     *
+     * @param fileType the new file type
+     */
+    public void setFileType(final FileType fileType) {
+        primaryKey.setFileType(fileType);
+    }
 
 }
--- src/main/java/org/guce/siat/common/model/UserAuthorityFileTypeId.java
+++ src/main/java/org/guce/siat/common/model/UserAuthorityFileTypeId.java
@@ -1,135 +1,118 @@
 package org.guce.siat.common.model;
 
 import java.io.Serializable;
-
 import javax.persistence.Embeddable;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-
 /**
  * The Class UserAuthorityFileTypeId.
  */
 @Embeddable
-public class UserAuthorityFileTypeId implements Serializable
-{
+public class UserAuthorityFileTypeId implements Serializable {
 
-	/** The Constant serialVersionUID. */
-	private static final long serialVersionUID = 662058985247014320L;
+    /**
+     * The Constant serialVersionUID.
+     */
+    private static final long serialVersionUID = 662058985247014320L;
 
-	/** The user authority. */
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "USER_AUTHORITY_ID", referencedColumnName = "ID", updatable = false)
-	private UserAuthority userAuthority;
+    /**
+     * The user authority.
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_AUTHORITY_ID", referencedColumnName = "ID", updatable = false)
+    private UserAuthority userAuthority;
 
-	/** The file type. */
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "FILE_TYPE_ID", referencedColumnName = "ID", updatable = false)
-	private FileType fileType;
+    /**
+     * The file type.
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "FILE_TYPE_ID", referencedColumnName = "ID", updatable = false)
+    private FileType fileType;
 
-	/**
-	 * Gets the user authority.
-	 *
-	 * @return the user authority
-	 */
-	public UserAuthority getUserAuthority()
-	{
-		return userAuthority;
-	}
+    /**
+     * Gets the user authority.
+     *
+     * @return the user authority
+     */
+    public UserAuthority getUserAuthority() {
+        return userAuthority;
+    }
 
-	/**
-	 * Sets the user authority.
-	 *
-	 * @param userAuthority
-	 *           the new user authority
-	 */
-	public void setUserAuthority(final UserAuthority userAuthority)
-	{
-		this.userAuthority = userAuthority;
-	}
+    /**
+     * Sets the user authority.
+     *
+     * @param userAuthority the new user authority
+     */
+    public void setUserAuthority(final UserAuthority userAuthority) {
+        this.userAuthority = userAuthority;
+    }
 
-	/**
-	 * Gets the file type.
-	 *
-	 * @return the file type
-	 */
-	public FileType getFileType()
-	{
-		return fileType;
-	}
+    /**
+     * Gets the file type.
+     *
+     * @return the file type
+     */
+    public FileType getFileType() {
+        return fileType;
+    }
 
-	/**
-	 * Sets the file type.
-	 *
-	 * @param fileType
-	 *           the new file type
-	 */
-	public void setFileType(final FileType fileType)
-	{
-		this.fileType = fileType;
-	}
+    /**
+     * Sets the file type.
+     *
+     * @param fileType the new file type
+     */
+    public void setFileType(final FileType fileType) {
+        this.fileType = fileType;
+    }
 
-	/*
+    /*
 	 * (non-Javadoc)
 	 *
 	 * @see java.lang.Object#hashCode()
-	 */
-	@Override
-	public int hashCode()
-	{
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((fileType == null) ? 0 : fileType.hashCode());
-		result = prime * result + ((userAuthority == null) ? 0 : userAuthority.hashCode());
-		return result;
-	}
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((fileType == null) ? 0 : fileType.hashCode());
+        result = prime * result + ((userAuthority == null) ? 0 : userAuthority.hashCode());
+        return result;
+    }
 
-	/*
+    /*
 	 * (non-Javadoc)
 	 *
 	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(final Object obj)
-	{
-		if (this == obj)
-		{
-			return true;
-		}
-		if (obj == null)
-		{
-			return false;
-		}
-		if (!(obj instanceof UserAuthorityFileTypeId))
-		{
-			return false;
-		}
-		final UserAuthorityFileTypeId other = (UserAuthorityFileTypeId) obj;
-		if (fileType == null)
-		{
-			if (other.fileType != null)
-			{
-				return false;
-			}
-		}
-		else if (!fileType.equals(other.fileType))
-		{
-			return false;
-		}
-		if (userAuthority == null)
-		{
-			if (other.userAuthority != null)
-			{
-				return false;
-			}
-		}
-		else if (!userAuthority.equals(other.userAuthority))
-		{
-			return false;
-		}
-		return true;
-	}
-
+     */
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof UserAuthorityFileTypeId)) {
+            return false;
+        }
+        final UserAuthorityFileTypeId other = (UserAuthorityFileTypeId) obj;
+        if (fileType == null) {
+            if (other.fileType != null) {
+                return false;
+            }
+        } else if (!fileType.equals(other.fileType)) {
+            return false;
+        }
+        if (userAuthority == null) {
+            if (other.userAuthority != null) {
+                return false;
+            }
+        } else if (!userAuthority.equals(other.userAuthority)) {
+            return false;
+        }
+        return true;
+    }
 
 }
