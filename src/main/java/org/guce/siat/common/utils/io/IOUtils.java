006fb56c4ce215cc9c4aac73920dc1cb1b2f9520
TADZOTSA Hyacinthe <romuald.tadzotsa@guichetunique.org>
2020-07-23 03:03:44 +0100

    move email file to error folder if send fails

--- src/main/java/org/guce/siat/common/utils/io/IOUtils.java
+++ src/main/java/org/guce/siat/common/utils/io/IOUtils.java
@@ -1,70 +1,58 @@
 package org.guce.siat.common.utils.io;
 
-
-
-import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-
+import java.io.InputStream;
+import java.io.OutputStream;
 import org.guce.siat.common.utils.ebms.Generator;
-
 
 /**
  * The Class IOUtils.
  */
-public final class IOUtils
-{
+public final class IOUtils {
 
-	private IOUtils()
-	{
+    private IOUtils() {
+    }
 
-	}
+    /**
+     * Write bytes to file.
+     *
+     * @param theFile the the file
+     * @param bytes the bytes
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
+    public static void writeBytesToFile(final File theFile, final byte[] bytes) throws IOException {
 
-	/**
-	 * Write bytes to file.
-	 *
-	 * @param theFile
-	 *           the the file
-	 * @param bytes
-	 *           the bytes
-	 * @throws IOException
-	 *            Signals that an I/O exception has occurred.
-	 */
-	public static void writeBytesToFile(final File theFile, final byte[] bytes) throws IOException
-	{
-		BufferedOutputStream bos = null;
+        try ( InputStream is = new ByteArrayInputStream(bytes);  OutputStream fos = new FileOutputStream(theFile)) {
+            org.apache.commons.io.IOUtils.copy(is, fos);
+        }
 
-		try
-		{
-			final FileOutputStream fos = new FileOutputStream(theFile);
-			bos = new BufferedOutputStream(fos);
-			bos.write(bytes);
-		}
-		finally
-		{
-			if (bos != null)
-			{
-				try
-				{
-					//flush and close the BufferedOutputStream
-					bos.flush();
-					bos.close();
-				}
-				catch (final Exception e)
-				{
-				}
-			}
-		}
-	}
+//        BufferedOutputStream bos = null;
+//
+//        try {
+//            FileOutputStream fos = new FileOutputStream(theFile);
+//            bos = new BufferedOutputStream(fos);
+//            bos.write(bytes);
+//        } finally {
+//            if (bos != null) {
+//                try {
+//                    //flush and close the BufferedOutputStream
+//                    bos.flush();
+//                    bos.close();
+//                } catch (final Exception e) {
+//                }
+//            }
+//        }
+    }
 
-	/**
-	 * Generate message id.
-	 *
-	 * @return the string
-	 */
-	public static String generateMessageID()
-	{
-		return Generator.generateMessageID();
-	}
+    /**
+     * Generate message id.
+     *
+     * @return the string
+     */
+    public static String generateMessageID() {
+        return Generator.generateMessageID();
+    }
 }
