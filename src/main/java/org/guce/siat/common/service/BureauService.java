a504a2aeb05becf788ada740ca1e9c18d640bd6b
TADZOTSA Hyacinthe <romuald.tadzotsa@guichetunique.org>
2020-07-29 07:49:58 +0100

    minor changes

--- src/main/java/org/guce/siat/common/service/BureauService.java
+++ src/main/java/org/guce/siat/common/service/BureauService.java
@@ -1,27 +1,21 @@
 package org.guce.siat.common.service;
 
 import java.util.List;
-
 import org.guce.siat.common.model.Bureau;
 import org.guce.siat.common.model.Organism;
 import org.guce.siat.common.utils.enums.BureauType;
 
-
-
 /**
  * The Interface BureauService.
  */
-public interface BureauService extends AbstractService<Bureau>
-{
+public interface BureauService extends AbstractService<Bureau> {
 
-	/**
-	 * Find bureau by type and organism.
-	 *
-	 * @param type
-	 *           the type
-	 * @param organism
-	 *           the organism
-	 * @return the list
-	 */
-	List<Bureau> findBureauByTypeAndOrganism(BureauType type, Organism organism);
+    /**
+     * Find bureau by type and organism.
+     *
+     * @param type the type
+     * @param organism the organism
+     * @return the list
+     */
+    List<Bureau> findBureauByTypeAndOrganism(BureauType type, Organism organism);
 }
