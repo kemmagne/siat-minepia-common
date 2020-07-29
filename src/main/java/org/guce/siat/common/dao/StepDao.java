a55f2f8b90c58f8c0d7954282d46b4f741cdffc3
TADZOTSA Hyacinthe <romuald.tadzotsa@guichetunique.org>
2020-07-29 08:36:06 +0100

    ok for findByStepCode after test

--- src/main/java/org/guce/siat/common/dao/StepDao.java
+++ src/main/java/org/guce/siat/common/dao/StepDao.java
@@ -1,25 +1,23 @@
 package org.guce.siat.common.dao;
 
 import java.util.List;
-
 import org.guce.siat.common.model.Authority;
 import org.guce.siat.common.model.Step;
-
-
+import org.guce.siat.common.utils.enums.StepCode;
 
 /**
  * The Interface StepDao.
  */
-public interface StepDao extends AbstractJpaDao<Step>
-{
+public interface StepDao extends AbstractJpaDao<Step> {
 
-	/**
-	 * Find by authoroties id.
-	 *
-	 * @param authority
-	 *           the id authority
-	 * @return the list
-	 */
-	List<Step> findByAuthority(Authority authority);
+    /**
+     * Find by authoroties id.
+     *
+     * @param authority the id authority
+     * @return the list
+     */
+    List<Step> findByAuthority(Authority authority);
+
+    Step findByStepCode(StepCode stepCode);
 
 }
--- src/main/java/org/guce/siat/common/dao/impl/StepDaoImpl.java
+++ src/main/java/org/guce/siat/common/dao/impl/StepDaoImpl.java
@@ -5,51 +5,63 @@ package org.guce.siat.common.dao.impl;
 
 import java.util.Collections;
 import java.util.List;
-
+import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
-
 import org.guce.siat.common.dao.StepDao;
 import org.guce.siat.common.model.Authority;
 import org.guce.siat.common.model.Step;
+import org.guce.siat.common.utils.enums.StepCode;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-
 
 /**
  * The Class StepDaoImpl.
  */
 @Repository("stepDao")
 @Transactional(propagation = Propagation.REQUIRED)
-public class StepDaoImpl extends AbstractJpaDaoImpl<Step> implements StepDao
-{
+public class StepDaoImpl extends AbstractJpaDaoImpl<Step> implements StepDao {
 
-	/**
-	 * Instantiates a new step dao impl.
-	 */
-	public StepDaoImpl()
-	{
-		super();
-		setClasse(Step.class);
-	}
+    /**
+     * Instantiates a new step dao impl.
+     */
+    public StepDaoImpl() {
+        super();
+        setClasse(Step.class);
+    }
 
 
-	/*
+    /*
 	 * (non-Javadoc)
 	 *
 	 * @see org.guce.siat.core.common.dao.StepDao#findByAuthorotiesId(org.guce.siat.common.model.Authority)
-	 */
-	@Override
-	public List<Step> findByAuthority(final Authority authority)
-	{
-		if (authority != null)
-		{
-			final String hqlString = "SELECT s FROM Step s INNER JOIN s.roleList rl WHERE rl.id = :authorityId";
-			final TypedQuery<Step> query = super.entityManager.createQuery(hqlString, Step.class);
-			query.setParameter("authorityId", authority.getId());
-			return query.getResultList();
-		}
-		return Collections.emptyList();
-	}
+     */
+    @Override
+    public List<Step> findByAuthority(final Authority authority) {
+        if (authority != null) {
+            final String hqlString = "SELECT s FROM Step s INNER JOIN s.roleList rl WHERE rl.id = :authorityId";
+            final TypedQuery<Step> query = super.entityManager.createQuery(hqlString, Step.class);
+            query.setParameter("authorityId", authority.getId());
+            return query.getResultList();
+        }
+        return Collections.emptyList();
+    }
+
+    @Override
+    public Step findByStepCode(StepCode stepCode) {
+
+        TypedQuery<Step> query = super.entityManager.createQuery("SELECT s FROM Step s WHERE s.stepCode = :stepCode", Step.class);
+
+        query.setParameter("stepCode", stepCode);
+
+        try {
+            return query.getSingleResult();
+        } catch (NoResultException nre) {
+            if (logger.isDebugEnabled()) {
+                logger.debug("There's no step with code {}", nre);
+            }
+            return null;
+        }
+    }
 
 }
