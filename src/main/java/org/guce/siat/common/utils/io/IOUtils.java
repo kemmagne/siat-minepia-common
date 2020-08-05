fc39881c88edbf603edd6406828de4375e6119f8
TADZOTSA Hyacinthe <romuald.tadzotsa@guichetunique.org>
2020-08-05 11:16:29 +0100

    change writeBytesToFile method on IOUtils

--- src/main/java/org/guce/siat/common/utils/io/IOUtils.java
+++ src/main/java/org/guce/siat/common/utils/io/IOUtils.java
@@ -1,11 +1,9 @@
 package org.guce.siat.common.utils.io;
 
-import java.io.ByteArrayInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import org.guce.siat.common.utils.ebms.Generator;
 
 /**
@@ -25,26 +23,25 @@ public final class IOUtils {
      */
     public static void writeBytesToFile(final File theFile, final byte[] bytes) throws IOException {
 
-        try ( InputStream is = new ByteArrayInputStream(bytes);  OutputStream fos = new FileOutputStream(theFile)) {
-            org.apache.commons.io.IOUtils.copy(is, fos);
-        }
-
-//        BufferedOutputStream bos = null;
-//
-//        try {
-//            FileOutputStream fos = new FileOutputStream(theFile);
-//            bos = new BufferedOutputStream(fos);
-//            bos.write(bytes);
-//        } finally {
-//            if (bos != null) {
-//                try {
-//                    //flush and close the BufferedOutputStream
-//                    bos.flush();
-//                    bos.close();
-//                } catch (final Exception e) {
-//                }
-//            }
+//        try ( InputStream is = new ByteArrayInputStream(bytes);  OutputStream fos = new FileOutputStream(theFile)) {
+//            org.apache.commons.io.IOUtils.copy(is, fos);
 //        }
+        BufferedOutputStream bos = null;
+
+        try {
+            FileOutputStream fos = new FileOutputStream(theFile);
+            bos = new BufferedOutputStream(fos);
+            bos.write(bytes);
+        } finally {
+            if (bos != null) {
+                try {
+                    //flush and close the BufferedOutputStream
+                    bos.flush();
+                    bos.close();
+                } catch (final Exception e) {
+                }
+            }
+        }
     }
 
     /**
