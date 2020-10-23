562d081e803f231881a74d39cf731dec30784000
TADZOTSA Hyacinthe <romuald.tadzotsa@guichetunique.org>
2020-10-23 17:47:04 +0100

    add the position CHEF_SECTEUR

--- src/main/java/org/guce/siat/common/utils/enums/PositionType.java
+++ src/main/java/org/guce/siat/common/utils/enums/PositionType.java
@@ -3,96 +3,107 @@ package org.guce.siat.common.utils.enums;
 /**
  * The Enum PositionType.
  */
-public enum PositionType
-{
+public enum PositionType {
 
-	/** The administrateur. */
-	ADMINISTRATEUR("ADMINISTRATEUR", "Administrateur", "Administrator"),
+    /**
+     * The administrateur.
+     */
+    ADMINISTRATEUR("ADMINISTRATEUR", "Administrateur", "Administrator"),
+    /**
+     * The ministre.
+     */
+    MINISTRE("MINISTRE", "Ministre", "Minister"),
+    /**
+     * The secretaire general.
+     */
+    SECRETAIRE_GENERAL("SECRETAIRE_GENERAL", "Secrétaire Général", "General Secretary"),
+    /**
+     * The directeur.
+     */
+    DIRECTEUR("DIRECTEUR", "Directeur", "Manager"),
+    /**
+     * The sous directeur.
+     */
+    SOUS_DIRECTEUR("SOUS_DIRECTEUR", "Sous Directeur", "Assistant Director"),
+    /**
+     * The chef service.
+     */
+    CHEF_SERVICE("CHEF_SERVICE", "Chef Service", "Head Service"),
+    /**
+     * The chef bureau.
+     */
+    CHEF_BUREAU("CHEF_BUREAU", "Chef Bureau", "Head Office"),
+    /**
+     * The chef secteur.
+     */
+    CHEF_SECTEUR("CHEF_SECTEUR", "Chef Secteur", "Head Sector"),
+    /**
+     * The agent.
+     */
+    AGENT("AGENT", "Agent", "Agent"),
+    /**
+     * The superviseur.
+     */
+    SUPERVISEUR("SUPERVISEUR", "Superviseur", "Supervisor"),
+    STATISTIQUE("STATISTIQUE", "Statistique", "Statistic"),
+    /**
+     * The observateur.
+     */
+    OBSERVATEUR("OBSERVATEUR", "Observateur", "Observer");
 
-	/** The ministre. */
-	MINISTRE("MINISTRE", "Ministre", "Minister"),
+    /**
+     * The code.
+     */
+    private final String code;
 
-	/** The secretaire general. */
-	SECRETAIRE_GENERAL("SECRETAIRE_GENERAL", "Secrétaire Général", "General Secretary"),
+    /**
+     * The label fr.
+     */
+    private final String labelFr;
 
-	/** The directeur. */
-	DIRECTEUR("DIRECTEUR", "Directeur", "Manager"),
+    /**
+     * The label en.
+     */
+    private final String labelEn;
 
-	/** The sous directeur. */
-	SOUS_DIRECTEUR("SOUS_DIRECTEUR", "Sous Directeur", "Assistant Director"),
+    /**
+     * Instantiates a new position type.
+     *
+     * @param code the code
+     * @param labelFr the label fr
+     * @param labelEn the label en
+     */
+    private PositionType(final String code, final String labelFr, final String labelEn) {
+        this.code = code.intern();
+        this.labelFr = labelFr.intern();
+        this.labelEn = labelEn.intern();
+    }
 
-	/** The chef service. */
-	CHEF_SERVICE("CHEF_SERVICE", "Chef Service", "Head Service"),
+    /**
+     * Gets the code.
+     *
+     * @return the code
+     */
+    public String getCode() {
+        return code;
+    }
 
-	/** The chef bureau. */
-	CHEF_BUREAU("CHEF_BUREAU", "Chef Bureau", "Head Office"),
+    /**
+     * Gets the label fr.
+     *
+     * @return the labelFr
+     */
+    public String getLabelFr() {
+        return labelFr;
+    }
 
-	/** The agent. */
-	AGENT("AGENT", "Agent", "Agent"),
-
-	/** The superviseur. */
-	SUPERVISEUR("SUPERVISEUR", "Superviseur", "Supervisor"),
-	STATISTIQUE("STATISTIQUE", "Statistique", "Statistic"),
-
-	/** The observateur. */
-	OBSERVATEUR("OBSERVATEUR", "Observateur", "Observer");
-
-
-	/** The code. */
-	private final String code;
-
-	/** The label fr. */
-	private final String labelFr;
-
-	/** The label en. */
-	private final String labelEn;
-
-
-	/**
-	 * Instantiates a new position type.
-	 *
-	 * @param code
-	 *           the code
-	 * @param labelFr
-	 *           the label fr
-	 * @param labelEn
-	 *           the label en
-	 */
-	private PositionType(final String code, final String labelFr, final String labelEn)
-	{
-		this.code = code.intern();
-		this.labelFr = labelFr.intern();
-		this.labelEn = labelEn.intern();
-	}
-
-	/**
-	 * Gets the code.
-	 *
-	 * @return the code
-	 */
-	public String getCode()
-	{
-		return code;
-	}
-
-	/**
-	 * Gets the label fr.
-	 *
-	 * @return the labelFr
-	 */
-	public String getLabelFr()
-	{
-		return labelFr;
-	}
-
-	/**
-	 * Gets the label en.
-	 *
-	 * @return the labelEn
-	 */
-	public String getLabelEn()
-	{
-		return labelEn;
-	}
+    /**
+     * Gets the label en.
+     *
+     * @return the labelEn
+     */
+    public String getLabelEn() {
+        return labelEn;
+    }
 
 }
