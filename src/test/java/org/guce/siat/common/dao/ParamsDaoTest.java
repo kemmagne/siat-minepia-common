e16cf3ce1987cbb57d27e9d717a7c4932b787598
TADZOTSA Hyacinthe <romuald.tadzotsa@guichetunique.org>
2020-10-26 20:11:02 +0100

    update ParamsDaoTest to use the test data builder

--- src/test/java/org/guce/siat/common/dao/ParamsDaoTest.java
+++ src/test/java/org/guce/siat/common/dao/ParamsDaoTest.java
@@ -2,9 +2,9 @@ package org.guce.siat.common.dao;
 
 import java.util.List;
 import junit.framework.TestCase;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.guce.siat.common.dao.config.H2DataSourceConfig;
 import org.guce.siat.common.model.Params;
-import org.guce.siat.common.utils.enums.ParamsCategory;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,52 +17,38 @@ import org.springframework.transaction.annotation.Transactional;
  *
  * @author ht
  */
+@Transactional
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {H2DataSourceConfig.class})
 public class ParamsDaoTest extends TestCase {
 
     @Autowired
-    private ParamsDao paramsDao;
+    private TestDataBuilder builder;
 
-    @Transactional
     @Test
     public void test_findParamsByName_notNull() {
-        String paramsName = "params.name";
-        String paramsValue = "params.value";
-        Params params = new Params();
-        params.setName(paramsName);
-        params.setValue(paramsValue);
-        paramsDao.save(params);
-        Params params1 = paramsDao.findParamsByName(paramsName);
+        Params params = builder.getParams();
+        Params params1 = builder.getParamsDao().findParamsByName(params.getName());
         Assert.assertNotNull(params1);
-        Assert.assertEquals(paramsName, params1.getName());
-        Assert.assertEquals(paramsValue, params1.getValue());
+        Assert.assertEquals(params.getName(), params1.getName());
+        Assert.assertEquals(params.getValue(), params1.getValue());
     }
 
-    @Transactional(readOnly = true)
     @Test
     public void test_findParamsByName_Null() {
-        String paramsName = "params.name2";
-        Params params = paramsDao.findParamsByName(paramsName);
+        String paramsName = RandomStringUtils.randomAlphabetic(5);
+        Params params = builder.getParamsDao().findParamsByName(paramsName);
         Assert.assertNull(params);
     }
 
-    @Transactional
     @Test
     public void test_findParamsByCategory() {
-        String paramsName = "params.name";
-        String paramsValue = "params.value";
-        ParamsCategory paramsCategory = ParamsCategory.GR;
-        Params params = new Params();
-        params.setName(paramsName);
-        params.setValue(paramsValue);
-        params.setParamsCategory(paramsCategory);
-        paramsDao.save(params);
-        List<Params> paramses = paramsDao.findParamsByCategory(paramsCategory);
+        Params params = builder.getParams();
+        List<Params> paramses = builder.getParamsDao().findParamsByCategory(params.getParamsCategory());
         Assert.assertFalse(paramses.isEmpty());
-        Params params1 = paramsDao.findParamsByName(paramsName);
-        Assert.assertEquals(paramsCategory, params1.getParamsCategory());
-        Assert.assertTrue(paramsDao.findParamsByCategory(null).isEmpty());
+        Params params1 = builder.getParamsDao().findParamsByName(params.getName());
+        Assert.assertEquals(params.getParamsCategory(), params1.getParamsCategory());
+        Assert.assertTrue(builder.getParamsDao().findParamsByCategory(null).isEmpty());
     }
 
 }
