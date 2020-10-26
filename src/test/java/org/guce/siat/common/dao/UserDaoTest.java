a49593355e2a89c5f5bbed871fc5972979d3fcb1
TADZOTSA Hyacinthe <romuald.tadzotsa@guichetunique.org>
2020-10-26 20:12:44 +0100

    create test to test the method findUsersByAuthorities of UserDao which actually use canonical classes and API Criteria

--- src/test/java/org/guce/siat/common/dao/UserDaoTest.java
+++ src/test/java/org/guce/siat/common/dao/UserDaoTest.java
@@ -3,17 +3,15 @@ package org.guce.siat.common.dao;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import org.apache.commons.lang3.RandomStringUtils;
+import junit.framework.TestCase;
 import org.guce.siat.common.dao.config.H2DataSourceConfig;
 import org.guce.siat.common.model.Administration;
 import org.guce.siat.common.model.Authority;
 import org.guce.siat.common.model.User;
 import org.guce.siat.common.model.UserAuthority;
 import org.guce.siat.common.utils.enums.AuthorityConstants;
-import org.guce.siat.common.utils.enums.AuthorityType;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,79 +23,78 @@ import org.springframework.transaction.annotation.Transactional;
  *
  * @author ht
  */
-@Transactional(readOnly = true)
+@Transactional
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {H2DataSourceConfig.class})
-public class UserDaoTest {
+public class UserDaoTest extends TestCase {
 
     private static final int NB_USERS = 10;
 
     @Autowired
-    private AdministrationDao administrationDao;
-    @Autowired
-    private AuthorityDao authorityDao;
-    @Autowired
-    private UserDao userDao;
-    @Autowired
-    private UserAuthorityDao userAuthorityDao;
+    private TestDataBuilder builder;
 
     private Administration administration;
     private Authority authority;
 
     @Before
+    @Override
     public void setUp() {
         Assert.assertTrue(NB_USERS % 2 == 0);
-        administration = new Administration();
-        administration.setLabelEn(RandomStringUtils.randomAlphanumeric(10));
-        administration.setLabelFr(RandomStringUtils.randomAlphanumeric(10));
-        administrationDao.save(administration);
+        administration = builder.getAdministration();
 
-        authority = new Authority();
-        authority.setId(1l);
-        authority.setAuthorityType(AuthorityType.DECISION);
-        authority.setLabelEn(AuthorityConstants.INSPECTEUR.getLabel());
-        authority.setLabelFr(AuthorityConstants.INSPECTEUR.getLabel());
-        authority.setRole(AuthorityConstants.INSPECTEUR.name());
-        authorityDao.save(authority);
+        authority = builder.getAuthority(AuthorityConstants.INSPECTEUR);
 
         for (int i = 0; i < NB_USERS; i++) {
-            User user = userDao.save(getUser());
+            User user = builder.getUser();
+            user.setAdministration(administration);
+            builder.getUserDao().update(user);
+
             user.setUserAuthorityList(new ArrayList<UserAuthority>());
             user.getUserAuthorityList().add(new UserAuthority(user, authority));
-            userAuthorityDao.saveList(user.getUserAuthorityList());
+            builder.getUserAuthorityDao().saveList(user.getUserAuthorityList());
         }
     }
 
-    @Ignore
     @Test
     public void test_findUsersByAdministrationAndAuthorities() {
-        List<User> users = userDao.findUsersByAdministrationAndAuthorities(Arrays.asList(administration.getId()), new String[]{authority.getRole()});
-        // all the users are disabled
-        Assert.assertEquals(0, users.size());
-        users = userDao.findActiveItems();
+        List<User> users = builder.getUserDao().findUsersByAdministrationAndAuthorities(Arrays.asList(administration.getId()), new String[]{authority.getRole()});
+        // all the users are enabled
+        Assert.assertEquals(NB_USERS, users.size());
+        users = builder.getUserDao().findActiveItems();
         int nb = users.size();
         Assert.assertTrue(nb == NB_USERS);
         for (int i = 0; i < NB_USERS; i++) {
             if (i % 2 == 0) {
-                users.get(i).setEnabled(true);
-                userDao.update(users.get(i));
+                users.get(i).setEnabled(false);
+                builder.getUserDao().update(users.get(i));
             }
         }
-        // some users are enabled
-        users = userDao.findActiveItems();
+        // some users are disabled
+        users = builder.getUserDao().findActiveItems();
         nb = users.size();
         Assert.assertTrue(nb == NB_USERS);
-        users = userDao.findUsersByAdministrationAndAuthorities(Arrays.asList(administration.getId()), new String[]{authority.getRole()});
+        users = builder.getUserDao().findUsersByAdministrationAndAuthorities(Arrays.asList(administration.getId()), new String[]{authority.getRole()});
         Assert.assertEquals(NB_USERS / 2, users.size());
     }
 
-    private User getUser() {
-
-        User user = new User();
-
-        user.setLogin(RandomStringUtils.randomAlphanumeric(10));
-        user.setAdministration(administration);
-
-        return user;
+    @Test
+    public void test_findUsersByAuthorities() {
+        List<User> users = builder.getUserDao().findAll();
+        Assert.assertFalse(users.isEmpty());
+        users = builder.getUserDao().findUsersByAuthorities(AuthorityConstants.INSPECTEUR.getCode());
+        Assert.assertFalse(users.isEmpty());
+        users = builder.getUserDao().findUsersByAuthorities(AuthorityConstants.CONTROLEUR.getCode());
+        Assert.assertTrue(users.isEmpty());
+        users = builder.getUserDao().findAll();
+        for (int i = 0; i < NB_USERS; i++) {
+            if (i % 2 == 0) {
+                users.get(i).setEnabled(false);
+                builder.getUserDao().update(users.get(i));
+            }
+        }
+        users = builder.getUserDao().findUsersByAuthorities(AuthorityConstants.INSPECTEUR.getCode());
+        Assert.assertFalse(users.isEmpty());
+        Assert.assertEquals(NB_USERS / 2, users.size());
     }
+
 }
