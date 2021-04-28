5c93af3f67c050efb5cbdc1d316253f9d7121cab
Hyacinthe Romuald TADZOTSA TIOMELA <romuald.tadzotsa@guichetunique.org>
2021-04-28 11:58:50 +0100

    update org.guce.siat.common.service.AbstractXmlConverterService to add a default implementation of the method saveReceivedFileAndAttachmentsAndExecuteFlow

--- src/main/java/org/guce/siat/common/service/AbstractXmlConverterService.java
+++ src/main/java/org/guce/siat/common/service/AbstractXmlConverterService.java
@@ -1,6 +1,13 @@
 package org.guce.siat.common.service;
 
+import java.io.IOException;
+import java.io.Serializable;
+import java.text.ParseException;
+import java.util.Map;
+import javax.persistence.PersistenceException;
+import org.apache.chemistry.opencmis.commons.exceptions.CmisConnectionException;
 import org.guce.siat.common.model.File;
+import org.guce.siat.common.utils.exception.ValidationException;
 
 /**
  *
@@ -10,6 +17,11 @@ public abstract class AbstractXmlConverterService implements XmlConverterService
 
     @Override
     public void rollbackFile(File destinationFile, File previousFile) {
+    }
+
+    @Override
+    public File saveReceivedFileAndAttachmentsAndExecuteFlow(Serializable document, Map<String, byte[]> attachementsMap) throws ParseException, PersistenceException, NullPointerException, ValidationException, CmisConnectionException, IOException {
+        return null;
     }
 
 }
