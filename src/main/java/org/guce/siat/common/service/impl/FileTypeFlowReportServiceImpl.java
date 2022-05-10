f5e07df9a3904e8427abc7d261dd05e1b1ab78f2
cissoko97 <boriscissoko@gmail.com>
2022-05-10 09:24:58 +0100

    update flow fileTypeFlowReport Service

--- src/main/java/org/guce/siat/common/service/impl/FileTypeFlowReportServiceImpl.java
+++ src/main/java/org/guce/siat/common/service/impl/FileTypeFlowReportServiceImpl.java
@@ -12,67 +12,67 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
-
 /**
  * The Class FileTypeFlowReportServiceImpl.
  */
 @Service("fileTypeFlowReportService")
 @Transactional(readOnly = true)
-public class FileTypeFlowReportServiceImpl extends AbstractServiceImpl<FileTypeFlowReport> implements FileTypeFlowReportService
-{
+public class FileTypeFlowReportServiceImpl extends AbstractServiceImpl<FileTypeFlowReport> implements FileTypeFlowReportService {
+
+    /**
+     * The file type flow report dao.
+     */
+    @Autowired
+    private FileTypeFlowReportDao fileTypeFlowReportDao;
+
+    /**
+     * Instantiates a new file type flow report service impl.
+     */
+    public FileTypeFlowReportServiceImpl() {
+        super();
+    }
 
 
-	/** The file type flow report dao. */
-	@Autowired
-	private FileTypeFlowReportDao fileTypeFlowReportDao;
-
-
-	/**
-	 * Instantiates a new file type flow report service impl.
-	 */
-	public FileTypeFlowReportServiceImpl()
-	{
-		super();
-	}
-
-
-	/*
+    /*
 	 * (non-Javadoc)
 	 * 
 	 * @see org.guce.siat.common.service.impl.AbstractServiceImpl#getJpaDao()
-	 */
-	@Override
-	public AbstractJpaDao<FileTypeFlowReport> getJpaDao()
-	{
-		return fileTypeFlowReportDao;
-	}
+     */
+    @Override
+    public AbstractJpaDao<FileTypeFlowReport> getJpaDao() {
+        return fileTypeFlowReportDao;
+    }
 
 
-	/*
+    /*
 	 * (non-Javadoc)
 	 * 
 	 * @see org.guce.siat.common.service.impl.AbstractServiceImpl#setJpaDao(org.guce.siat.common.dao.AbstractJpaDao)
-	 */
-	@Override
-	public void setJpaDao(final AbstractJpaDao<FileTypeFlowReport> jpaDao)
-	{
-		this.fileTypeFlowReportDao = (FileTypeFlowReportDao) jpaDao;
-	}
+     */
+    @Override
+    public void setJpaDao(final AbstractJpaDao<FileTypeFlowReport> jpaDao) {
+        this.fileTypeFlowReportDao = (FileTypeFlowReportDao) jpaDao;
+    }
 
-
-
-
-	/*
+    /*
 	 * (non-Javadoc)
 	 * 
 	 * @see
 	 * org.guce.siat.common.service.FileTypeFlowReportService#findReportClassNameByFlowAndFileType(org.guce.siat.common
 	 * .model.Flow, org.guce.siat.common.model.FileType)
-	 */
-	@Override
-	public List<FileTypeFlowReport> findReportClassNameByFlowAndFileType(final Flow flow, final FileType fileType)
-	{
-		return fileTypeFlowReportDao.findReportClassNameByFlowAndFileType(flow, fileType);
-	}
+     */
+    @Override
+    public List<FileTypeFlowReport> findReportClassNameByFlowAndFileType(final Flow flow, final FileType fileType) {
+        return fileTypeFlowReportDao.findReportClassNameByFlowAndFileType(flow, fileType);
+    }
+
+    @Override
+    public FileTypeFlowReport findReportClassNameByFileType(FileType fileType) {
+        return fileTypeFlowReportDao.findReportClassNameByFileType(fileType);
+    }
+
+    @Override
+    public FileTypeFlowReport findReportClassNameByFileFieldName(String fileFieldName) {
+        return fileTypeFlowReportDao.findReportClassNameByFileFieldName(fileFieldName);
+    }
 }
