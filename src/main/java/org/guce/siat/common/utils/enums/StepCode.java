34488cc7dccf4bdea24bd838e51cc4e3455469e6
TONYE LISSOUCK Pierre Nicolas Eric <eric.tonye@guichetunique.org>
2022-07-15 15:46:38 +0100

    Intégration des demandes de prorogations de l'autorisation d'importation MINMIDT

--- src/main/java/org/guce/siat/common/utils/enums/StepCode.java
+++ src/main/java/org/guce/siat/common/utils/enums/StepCode.java
@@ -312,6 +312,7 @@ public enum StepCode {
     ST_AP_65("Paiement"),
     ST_AP_VT1_01("Signature Modification du visa techique"),
     ST_AP_AIM_01("Signature Modification de l'autorisation d'importation"),
+    ST_AP_AIM_02("Signature Prorogation de l'autorisation d'importation"),
     // CO Step Code
     /**
      * The S t_ c o_42.
